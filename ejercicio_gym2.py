62a9cb4e1aaff56e6ae790a97a021106e961306f
Zelton <108957790+Zelton@users.noreply.github.com>
2026-01-23 14:37:32 +0000

    Ejercicio app gym 2 finalizado

--- ejercicio_gym2.py
+++ ejercicio_gym2.py
@@ -7,3 +7,7 @@ print("=====ENTRENAMIENTO 2 =====")
 print('''Ejercicio: Peso muerto,
 Peso: 120kg
 Reps:5''')
+
+#EJERCICIO 2.1 CHATGPT
+
+print("===== PIERNA DIA 1 =====\nEjercicio: Sentadilla\nPeso: 80kg\nSeries: 4\nReps: 8")
