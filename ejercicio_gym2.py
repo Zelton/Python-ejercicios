9e76062acdf7f4d7d11a69c3c62eb878803db730
Zelton <108957790+Zelton@users.noreply.github.com>
2026-01-23 14:12:40 +0000

    Ejercicio gym 2 ChatGPT finalizado

---
+++ ejercicio_gym2.py
@@ -0,0 +1,9 @@
+print('===== ENTRENAMIETO 1 ======')
+print("""Ejercicio: Dominadas 
+Series: 4
+Reps: 8""")
+
+print("=====ENTRENAMIENTO 2 =====")
+print('''Ejercicio: Peso muerto,
+Peso: 120kg
+Reps:5''')
