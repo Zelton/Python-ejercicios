c7f02eb932e72c0dea3f681b89959fa7649909b9
Zelton <108957790+Zelton@users.noreply.github.com>
2026-01-23 16:59:00 +0000

    gym2 funcionando

---
+++ gym2.py
@@ -0,0 +1,14 @@
+nombre_usuario = "Adrián"
+edad_usuario = 35
+ciudad_usuario = "Madrid"
+altura_usuario = "178cm"
+
+print("===== FICHA DE USUARIO =====")
+print("Nombre: ", end = "")
+print(nombre_usuario)
+print("Edad: ", end = "")
+print(edad_usuario)
+print("Ciudad: ", end = "")
+print(ciudad_usuario)
+print("Alturas: ", end = "")
+print(altura_usuario)
