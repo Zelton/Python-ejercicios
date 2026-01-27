5a8e2d9a7626040cc0a549768e69289c8d86be28
Zelton <108957790+Zelton@users.noreply.github.com>
2026-01-27 16:04:55 +0000

    resumen1 finalizado

---
+++ resumen1.py
@@ -0,0 +1,13 @@
+# F-string
+nombre = "Adrián"
+age = 31
+musica = "rock"
+ciudad = "Salamanca"
+mascota1 = "Kiara"
+mascota2 = "Arai"
+sueldo = "1445"
+
+print(f"Hola, me llamo {nombre}, en 4 años tendré {age + 4}\n"
+f"Vivo en {ciudad} y me encanta escuchar {musica}\n"
+f"Tengo dos mascotas {mascota1} y {mascota2}\n"
+f"Mi salario hace unos meses era {int(sueldo) -100}")
