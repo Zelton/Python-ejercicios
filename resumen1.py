1063a6ce592c1ab7036bebce193c8d09954a18c8
Zelton <108957790+Zelton@users.noreply.github.com>
2026-01-27 16:24:39 +0000

    resumen1 finalizado

--- resumen1.py
+++ resumen1.py
@@ -11,3 +11,7 @@ print(f"Hola, me llamo {nombre}, en 4 años tendré {age + 4}\n"
 f"Vivo en {ciudad} y me encanta escuchar {musica}\n"
 f"Tengo dos mascotas {mascota1} y {mascota2}\n"
 f"Mi salario hace unos meses era {int(sueldo) -100}")
+
+# F-strings y funcion input()
+
+ print(f"Hola, ¿cómo te llamas?")
